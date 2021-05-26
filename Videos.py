de03c137344ccf1239b11822a7b76453fb9e1b3a
bob34-byte <78810546+bob34-byte@users.noreply.github.com>
2021-05-26 13:51:18 -0400

    Create Videos.py

---
+++ Videos.py
@@ -0,0 +1,15 @@
+import gevent
+from flask import copy_current_request_context
+
+@app.route('/')
+def index():
+    @copy_current_request_context
+    def do_some_work():
+        # do some work here, it can access flask.request or
+        # flask.session like you would otherwise in the view function.
+        ...
+    gevent.spawn(do_some_work)
+    return 'Regular response
+    @app.route('/videos')
+    def videos():
+         return "hello"
