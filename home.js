bd14e33836cff70a1df2ffd1892a5795989f0dd2
bob34-byte <78810546+bob34-byte@users.noreply.github.com>
2021-05-26 09:58:46 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
 alert("hello");
-if(window.location.href = window.location.href + "/?page=learnhtml"){
+if(window.location.href = window.location.href +"/?page=learnhtml"){
   window.location.href = "learnhtml.html";
 }
